420cc44ef5771d26eedf0b2e45b1a50847b37afd
lancejethro <sanpablolancejethro@gmail.com>
2024-07-16 15:42:00 +0800

    addded functions

--- Program.cs
+++ Program.cs
@@ -1,18 +1,14 @@
 ﻿internal class Program
 {
-    private static void Main(string[] args)
+    public static void loadingScreen()
     {
-        // bool x = true;
-        // while (x == true)
-        // {
-
         Console.WriteLine();
         Console.WriteLine();
         Console.WriteLine();
         Console.WriteLine(@"   _______/\\\\\______        __/\\\________/\\\_        __/\\\\\\\\\\\\\\\_        _____/\\\\\\\\\____ ");
         Console.WriteLine(@"    _____/\\\///\\\____        _\/\\\_____/\\\//__        _\/\\\///////////__        ___/\\\\\\\\\\\\\__       ");
         Console.WriteLine(@"     ___/\\\/__\///\\\__        _\/\\\__/\\\//_____        _\/\\\_____________        __/\\\/////////\\\_    ");
-        Thread.Sleep(1000);
+        Thread.Sleep(500);
         Console.Clear();
         Console.WriteLine();
         Console.WriteLine();
@@ -25,7 +21,7 @@
         Console.WriteLine(@"          __/\\\______\//\\\_        _\/\\\\\\//\\\_____        _\/\\\\\\\\\\\_____        _\/\\\_______\/\\\_     ");
         Console.WriteLine(@"           _\/\\\_______\/\\\_        _\/\\\//_\//\\\____        _\/\\\///////______        _\/\\\\\\\\\\\\\\\_    ");
         Console.WriteLine(@"            _\//\\\______/\\\__        _\/\\\____\//\\\___        _\/\\\_____________        _\/\\\/////////\\\_   ");
-        Thread.Sleep(1000);
+        Thread.Sleep(500);
         Console.Clear();
         Console.WriteLine();
         Console.WriteLine();
@@ -43,7 +39,7 @@
         Console.WriteLine(@"                 __\///\\\__/\\\____        _\/\\\_____\//\\\__        _\/\\\_____________        _\/\\\_______\/\\\_  ");
         Console.WriteLine(@"                  ____\///\\\\\/_____        _\/\\\______\//\\\_        _\/\\\\\\\\\\\\\\\_        _\/\\\_______\/\\\_ ");
         Console.WriteLine(@"                   ______\/////_______        _\///________\///__        _\///////////////__        _\///________\///__");
-        Thread.Sleep(1000);
+        Thread.Sleep(500);
         Console.Clear();
         Console.WriteLine();
         Console.WriteLine();
@@ -63,7 +59,7 @@
         Console.WriteLine(@"                        _\///\\\__/\\\____        _\/\\\_____\//\\\__        _\/\\\_____________        _\/\\\_______\/\\\_  ");
         Console.WriteLine(@"                        ____\///\\\\\/_____        _\/\\\______\//\\\_        _\/\\\\\\\\\\\\\\\_        _\/\\\_______\/\\\_ ");
         Console.WriteLine(@"                         ______\/////_______        _\///________\///__        _\///////////////__        _\///________\///__");
-        Thread.Sleep(1000);
+        Thread.Sleep(500);
         Console.Clear();
         Console.WriteLine();
         Console.WriteLine();
@@ -84,8 +80,33 @@
         Console.WriteLine(@"                                    _\///\\\__/\\\____        _\/\\\_____\//\\\__        _\/\\\_____________        _\/\\\_______\/\\\_  ");
         Console.WriteLine(@"                                    ____\///\\\\\/_____        _\/\\\______\//\\\_        _\/\\\\\\\\\\\\\\\_        _\/\\\_______\/\\\_ ");
         Console.WriteLine(@"                                     ______\/////_______        _\///________\///__        _\///////////////__        _\///________\///__");
-        Thread.Sleep(1000);
+        Thread.Sleep(500);
+    }
 
+    public static void Login()
+    {
+        string? userName;
+        string? passWord;
+        Console.WriteLine("\t\t\t\t\t\t==================================================================");
+        Console.WriteLine("\t\t\t\t\t\t||                                                              ||");
+        Console.WriteLine("\t\t\t\t\t\t||                                                              ||");
+        Console.WriteLine("\t\t\t\t\t\t||                                                              ||");
+        Console.WriteLine("\t\t\t\t\t\t||\t\t LOGIN Page\t\t\t\t\t||");
+        Console.WriteLine("\t\t\t\t\t\t||                                                              ||");
+        Console.WriteLine("\t\t\t\t\t\t||                                                              ||");
+        Console.Write("\t\t\t\t\t\t||\t Enter Username: ");
+        userName = Console.ReadLine();
+        Console.Write("\t\t\t\t\t\t||\tEnter Password: ");
+        passWord = Console.ReadLine();
+        Console.WriteLine("\t\t\t\t\t\t||                                                              ||");
+        Console.WriteLine("\t\t\t\t\t\t||                                                              ||");
+        Console.WriteLine("\t\t\t\t\t\t==================================================================");
+    }
+    private static void Main(string[] args)
+    {
+
+        //tawagin loading screen method
+        loadingScreen();
         Console.WriteLine("\n\n\n\n");
         Console.WriteLine("\t\t\t\t\t\t==================================================================");
         Console.WriteLine("\t\t\t\t\t\t||                                                              ||");
@@ -100,10 +121,10 @@
         Console.WriteLine("\t\t\t\t\t\t||                                                              ||");
         Console.WriteLine("\t\t\t\t\t\t||                                                              ||");
         Console.WriteLine("\t\t\t\t\t\t==================================================================");
-        String ans;
+
+        String? ans;
         Console.Write("\t\t\t\t\t\tChoose action here: \t\t");
         ans = Convert.ToString(Console.ReadLine());
-
 
         switch (ans)
         {
@@ -114,13 +135,13 @@
                 // x = false;
                 Console.ReadKey();
                 break;
-            case "2":
+            case "1":
                 Console.Clear();
                 // function papunta sa view inventory
                 break;
-            case "1":
+            case "2":
                 Console.Clear();
-                // function papunta sa login
+                Login();
 
                 break;
             default:
--- obj/Debug/net8.0/JethroPart.AssemblyInfo.cs
+++ obj/Debug/net8.0/JethroPart.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JethroPart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f806d7a8ca86de5a1c3fa2372986aa0f944796d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afe245a7b205ad59aebc57215b477ca6bb219dde")]
 [assembly: System.Reflection.AssemblyProductAttribute("JethroPart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JethroPart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
